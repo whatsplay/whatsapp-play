21e1dac6ebae9aa09ff0ef68c5508e2bc61f6424
xandao6 <alexandrecalilmf@gmail.com>
2020-01-05 01:05:59 -0300

    Implemented seleniumUtils in wchat

--- wplay/wchat.py
+++ wplay/wchat.py
@@ -1,41 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from wplay import seleniumUtils as sel
+
 
 def chat(name):
 
-	# chrome driver
-	driver = webdriver.Chrome(ChromeDriverManager().install())
-	driver.get("https://web.whatsapp.com/")
-	wait = WebDriverWait(driver, 600)
+    _, driver_wait, chosen_website = sel.initialize_chrome_driver(
+        sel.websites['whatsapp'])
 
-	condition=True
-	while condition:
-		print("1. Chat")
-		print("2. Quit")
-		option = int(input("Enter choice: "))
-		# if option 1 is selected
-		if option==1:
-			# enter the name of the person by the user
-			receiver =  str(name) #str(input("Enter the name of receiver: "))
-			message = str(input("Enter your message: "))
+    condition = True
+    while condition:
+        print("1. Chat")
+        print("2. Quit")
+        option = int(input("Enter choice: "))
 
-			# finds the person and navigate to it
-			x_arg = '//span[contains(@title, '+ '"' +receiver + '"'+ ')]'
-			person_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-			print(receiver)
-			person_title.click()
+        if option == 1:
+            # enter the name of the person by the user
+            target = str(name)  # str(input("Enter the name of target: "))
+            message = str(input("Enter your message: "))
 
-			# navigate to text part
-			xpath = '//div[@class="_3u328 copyable-text selectable-text"]'
-			message_area = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+            sel.find_and_navigate_to_target(
+                driver_wait, chosen_website, target)
 
-			# sends message
-			message_area.send_keys(message + Keys.ENTER)
-		# if option 2 is selected
-		if option==2:
-			condition=False
-			exit()
+            message_area = sel.navigate_to_message_area(
+                driver_wait, chosen_website)
+
+            sel.send_message(message_area, message)
+
+        if option == 2:
+            condition = False
+            exit()
