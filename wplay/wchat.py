a8d6c1db1874f3d3d8560cb5d6c756036fe09ff0
xandao6 <alexandrecalilmf@gmail.com>
2020-01-06 21:50:33 -0300

    improved wchat script (can still be optimized)

--- wplay/wchat.py
+++ wplay/wchat.py
@@ -2,23 +2,20 @@ from wplay import seleniumUtils as sel
 
 
 def chat(name):
-
     _, driver_wait, chosen_website = sel.initialize_chrome_driver(
         sel.websites['whatsapp'])
 
-    condition = True
-    while condition:
+    while True:
         print("1. Chat")
         print("2. Quit")
         option = int(input("Enter choice: "))
 
         if option == 1:
-            # enter the name of the person by the user
-            target = str(name)  # str(input("Enter the name of target: "))
+            #name = str(input("Enter the name of target: "))
             message = str(input("Enter your message: "))
 
             sel.find_and_navigate_to_target(
-                driver_wait, chosen_website, target)
+                driver_wait, chosen_website, name)
 
             message_area = sel.navigate_to_message_area(
                 driver_wait, chosen_website)
@@ -26,5 +23,4 @@ def chat(name):
             sel.send_message(message_area, message)
 
         if option == 2:
-            condition = False
             exit()
