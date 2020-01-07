f87e9de6f77589fbb2912cceaee30d1b71f2748f
xandao6 <alexandrecalilmf@gmail.com>
2020-01-07 01:06:56 -0300

    onlinetracker script rewrited

--- wplay/onlinetracker.py
+++ wplay/onlinetracker.py
@@ -1,66 +1,73 @@
 import time
 import os
-import datetime
+import subprocess
+from datetime import datetime
 from playsound import playsound
 from wplay import seleniumUtils as sel
 
-def tracker(name):
 
-    # the name of the person by the user
-    target = str(name) #str(input("Enter the name of target: "))
+def tracker(name):
+    #name = str(input("Enter the name of target: "))
 
     driver, driver_wait, chosen_website = sel.initialize_chrome_driver(
         sel.websites['whatsapp'])
 
-    sel.find_and_navigate_to_target(driver_wait, chosen_website, target)
-
-    NoSuchElementException, \
-		StaleElementReferenceException = sel.get_selenium_exceptions()
+    sel.find_and_navigate_to_target(driver_wait, chosen_website, name)
 
     # finds if online_status directory is present
-    if 'online_status_data' not in os.listdir(os.getcwd()):
-        os.mkdir('online_status_data')
-    f=open(os.path.join('online_status_data' , 'status.txt'),'w')
-    f.close()
+    if 'tracking_data' not in os.listdir(os.getcwd()):
+        os.mkdir('tracking_data')
+
+    # create status.txt file and overwrite if exists
+    status_file = open(
+        os.path.join('tracking_data', f'status_{name}.txt'), 'w')
+    status_file.close()
+
+    # open status.txt in memory with append mode
+    status_file = open(
+        os.path.join('tracking_data', f'status_{name}.txt'), 'a')
+
     # check status
-    while True:
-        i=0
-        try:
-            status = driver.find_element_by_class_name('_315-i').text
-            i=1
-        except (NoSuchElementException, StaleElementReferenceException):
-            status = 'offline'
-            i=0
-        # to play sound when the person is online
-        if i==1:
-            try:
-                playsound('plucky.wav')
-            except:
-                print("Error: Couldn't play the sound. \
-                But the person is online!")
-        # prints date, time and status
-        print(datetime.datetime.now())
-        print(status)
-        # writes date, time and status in status.txt
-        f=open(os.path.join('online_status_data' , 'status.txt'),'a')
-        f.write(str(datetime.datetime.now()))
-        f.write(status)
-        f.close()
-        # Loop to check the online status until the offline status appear and vice-versa
-        # this loop will help to stop print status until the other status appears
+    is_sound_enabled = True
+    last_status = 'offline'
+    try:
         while True:
-            if i == 1:
-                try:
-                    re_status = driver.find_element_by_class_name('_315-i').text
-                    re_status = 'online'
-                    continue
-                except (NoSuchElementException, StaleElementReferenceException):
-                    break
-            else:
-                try:
-                    re_status = driver.find_element_by_class_name('_315-i').text
-                    re_status = 'online'
-                    break
-                except (NoSuchElementException, StaleElementReferenceException):
-                    continue
-        time.sleep(1)
+            try:
+                status = driver.find_element_by_class_name('_315-i').text
+                if status == 'online':
+                    is_online = True
+                else:
+                    # status is last seen
+                    is_online = False
+                    status = 'offline'
+            except:
+                status = 'offline'
+                is_online = False
+
+            if last_status != is_online:
+                # play sound when the person is online
+                if is_online == True:
+                    try:
+                        if is_sound_enabled == True:
+                            playsound('plucky.wav')
+                    except:
+                        print("Error: Couldn't play the sound.")
+                        is_sound_enabled = False
+
+                # print date, time and status to console
+                print(
+                    f'{datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}' +
+                    f' - Status: {status}'
+                )
+
+                # writes date, time and status in status.txt
+                status_file.write(
+                    f'{datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}' +
+                    f' - Status: {status}\n')
+
+            last_status = is_online
+            time.sleep(0.5)
+    finally:
+        status_file.close()
+        print(f'\nStatus file saved in: ' +
+              f'{os.path.join(os.getcwd(),"tracking_data")}')
