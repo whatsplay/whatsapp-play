7b74f9d6ecc952c92cdbb1cabbc2fbfaca94d4be
xandao6 <alexandrecalilmf@gmail.com>
2020-01-06 21:59:20 -0300

    changed identation from tab to spaces

--- wplay/locationfinder.py
+++ wplay/locationfinder.py
@@ -2,12 +2,12 @@
 
 def locationfinder(name):
 
-	"""
-	# to find location by ip address
-	print('Get you ipinfo token from https://ipinfo.io/account')
-	ip_address = '*'
-	token = str(input("Enter your ipinfo token: "))
-	ip_string = 'curl ipinfo.io/'+ip_address+'?token='+token+''
-	os.system(ip_string)
-	"""
+    """
+    # to find location by ip address
+    print('Get you ipinfo token from https://ipinfo.io/account')
+    ip_address = '*'
+    token = str(input("Enter your ipinfo token: "))
+    ip_string = 'curl ipinfo.io/'+ip_address+'?token='+token+''
+    os.system(ip_string)
+    """
 
