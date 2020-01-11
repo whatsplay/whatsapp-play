6aba4b400e24db4f7174e884f7e2ce90794899d8
xandao6 <alexandrecalilmf@gmail.com>
2020-01-11 19:34:37 -0300

    added pyppeteerCookies

---
+++ wplay/pyppeteerCookies.py
@@ -0,0 +1,19 @@
+__author__ = 'Alexandre Calil Martins Fonseca, github: xandao6'
+
+
+# region TUTORIAL
+'''
+Go to region 'FOR SCRIPTING' and use the methods in your script!
+
+EXAMPLE OF USAGE:
+from wplay import pyppeteerCookies as cookies
+
+'''
+# endregion
+
+
+# region TODO and FIXME
+'''
+
+'''
+# endregion
