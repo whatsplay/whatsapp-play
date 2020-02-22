8e99af2ac67a5622acbc91f3cfcb7c6a681fa72a
Alexandre Calil <alexandrecalilmf@gmail.com>
2020-02-22 02:17:24 -0300

    selector updated

--- wplay/utils/target_data.py
+++ wplay/utils/target_data.py
@@ -8,7 +8,7 @@ from wplay.utils.helpers import whatsapp_selectors_dict, websites
 async def get_last_seen_from_focused_target(page):
     #await page.waitForSelector(whatsapp_selectors_dict['status'], visible = True)
     try:
-        status = await page.evaluate(f'document.querySelector("{whatsapp_selectors_dict["status"]}").getAttribute("title")')
+        status = await page.evaluate(f'document.querySelector("{whatsapp_selectors_dict["last_seen"]}").getAttribute("title")')
         return status
     except:
         return '#status not found'
