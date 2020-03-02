44e941a007e433f70e07dd72b973fb24727ec376
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-03-02 22:20:14 +0530

    code style fixes

--- wplay_gui/wplay/public/electron.js
+++ wplay_gui/wplay/public/electron.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const path = require("path")
-const isDev = require("electron-is-dev")
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 function createWindow () {
   // Create the browser window.
@@ -10,12 +10,12 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
   win.loadURL(
     isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
-  )
+  );
 
   // Open the DevTools.
   // win.webContents.openDevTools()
@@ -24,21 +24,21 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
--- wplay_gui/wplay/src/App.js
+++ wplay_gui/wplay/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
