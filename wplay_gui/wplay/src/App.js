ddd45cf5a5c7681aede49d9769c46274f1d44df2
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-03-02 21:43:04 +0530

    wplay gui initial setup

---
+++ wplay_gui/wplay/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Wplay GUI</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
