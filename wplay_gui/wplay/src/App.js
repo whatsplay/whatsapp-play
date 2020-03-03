30f015c88a4c184bb43b26791c1d41cd6a7c58b6
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-03-03 12:47:34 +0530

    name changed

--- wplay_gui/wplay/src/App.js
+++ wplay_gui/wplay/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Wplay GUI</h1>
+        <h1>whatsapp-play</h1>
       </header>
     </div>
   );
