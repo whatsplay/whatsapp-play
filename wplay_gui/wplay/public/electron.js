4cc13fa2a335b6766d64c5572984f93476d80794
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-03-04 07:20:07 +0530

    wplay-gui initial setup issue #43 (#172)
    * wplay gui initial setup
    
    * wplay gui project readme
    
    * code style fixes
    
    * name changed

---
+++ wplay_gui/wplay/public/electron.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  win.loadURL(
+    isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
