54ec4f4e81da64c5526759bca5b96c16016bea46
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 09:59:04 +0530

    Update messageblast.py

--- wplay/messageblast.py
+++ wplay/messageblast.py
@@ -1,12 +1,21 @@
+# Importing the various python files present in util folder of wplay
+
 from wplay.utils import browser_config
 from wplay.utils import target_search
 from wplay.utils import io
 
+# This function gets the input which is username as given by the client and checks for chromedriver to load Whatsapp web.
+
 
 async def blast(target):
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
+    # It searches the username by putting the name in search bar by using divison selectors and if more username found asks for your target.
     await target_search.search_and_select_target(page, target)
+    
     message = io.ask_user_for_message_breakline_mode()
+    
     number_of_messages = int(input("Enter the number of messages to blast: "))
+    
+    # Finally it asks for no of message and timing of the messages to the target.
     for _ in range(number_of_messages):
         await io.send_message(page, message)
