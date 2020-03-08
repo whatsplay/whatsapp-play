cd641b50dcae4a7aa62b5efe97f418dd58d9f5c7
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 10:37:00 +0530

    Update messageblast.py

--- wplay/messageblast.py
+++ wplay/messageblast.py
@@ -9,13 +9,13 @@ from wplay.utils import io
 
 async def blast(target):
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
-    # It searches the username by putting the name in search bar by using divison selectors and if more username found asks for your target.
+# It searches the username by putting the name in search bar by using divison selectors and if more username found asks for your target.
     await target_search.search_and_select_target(page, target)
     
     message = io.ask_user_for_message_breakline_mode()
     
     number_of_messages = int(input("Enter the number of messages to blast: "))
     
-    # Finally it asks for no of message and timing of the messages to the target.
+# Finally it asks for no of message and timing of the messages to the target.
     for _ in range(number_of_messages):
         await io.send_message(page, message)
