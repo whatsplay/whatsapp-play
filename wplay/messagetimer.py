537d76dff3930c1e4f974a26ba664e9f27be4b7b
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 10:03:35 +0530

    Update messagetimer.py

--- wplay/messagetimer.py
+++ wplay/messagetimer.py
@@ -1,14 +1,17 @@
+# First two lines import python libraries
 import time
 import random
 
+#These lines import functions from utils pyton file in wplay.
 from wplay.utils import browser_config
 from wplay.utils import target_search
 from wplay.utils import io
 
-
+# This function first checks for proper whatsapp web loading.
 async def msgTimer(target):
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
 
+    # Waiting till the target username is found.
     await target_search.search_and_select_target(page, target)
 
     # Region INPUTS
@@ -17,7 +20,9 @@ async def msgTimer(target):
     messages = list()
     for _ in range(message_type_numbers):
         messages.append(io.ask_user_for_message_breakline_mode())
+    # Asking the client for no of message inputs.
     number_of_messages = int(input("Enter the number of messages to send: "))
+    # The interval of inputs that is time after which the messages are to be send
     minimumTimeInterval = int(
         input("Enter minimum interval number in seconds: "))
     maximumTimeInterval = int(
@@ -33,3 +38,4 @@ async def msgTimer(target):
             time.sleep(random.randrange(minimumTimeInterval, maximumTimeInterval))
         else:
             time.sleep(minimumTimeInterval)
+# This script is a helper script for message blast script as the no of messages and the message time are defined here and are being using in the blast script.
