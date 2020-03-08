067a03686204768034572939ad41dd5d83a8280d
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 09:35:09 +0530

    Update kill_process.py

--- wplay/utils/kill_process.py
+++ wplay/utils/kill_process.py
@@ -1,7 +1,9 @@
+#These lines import signal library for asynchronous event handling along with psutil for process and system utility.
+
 import signal
 import psutil
 
-
+# Checks for the event handler by using exception handling with try and except if still not working then prints process killed.
 def kill_child_processes(parent_pid, sig=signal.SIGTERM):
     try:
         parent = psutil.Process(parent_pid)
