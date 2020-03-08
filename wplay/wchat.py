1659ca9d23ee8a9e2f7cabccd503d7f70229bf7d
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 10:22:02 +0530

    Update wchat.py

--- wplay/wchat.py
+++ wplay/wchat.py
@@ -1,13 +1,15 @@
+# These are imports from util folder of wplay
 from wplay.utils import browser_config
 from wplay.utils import target_search
 from wplay.utils import io
 
-
+# First checks for the target which is the username as entered by the client after running wplay on a command line script
 async def chat(target):
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
 
     await target_search.search_and_select_target(page, target)
-
+    
+    # If all the configurations are correct whatsapp web is loaded and target found then ask for message and send message.
     while True:
         message = io.ask_user_for_message_breakline_mode()
         await io.send_message(page, message)
