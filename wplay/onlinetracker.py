7f39fa9e35601d7b1196cd540ea68baaceb3a878
Atul Kumar <32452535+07ATUL@users.noreply.github.com>
2020-03-08 10:53:56 +0530

    Update onlinetracker.py

--- wplay/onlinetracker.py
+++ wplay/onlinetracker.py
@@ -3,13 +3,11 @@ import time
 from pathlib import Path
 from datetime import datetime
 from playsound import playsound
-
 # These import from util folder in wplay
 from wplay.utils import browser_config
 from wplay.utils import target_search
 from wplay.utils import target_data
 from wplay.utils.helpers import data_folder_path
-
 # After the client run the command line script, the user enters a name and that is the target which is used here.
 async def tracker(target):
 # Waiting for Whatsapp web to be loading
@@ -17,18 +15,14 @@ async def tracker(target):
 # Searching for the target name 
     target_name = await target_search.search_and_select_target(page, target, hide_groups = True)
     Path(data_folder_path / 'tracking_data').mkdir(parents = True, exist_ok = True)
-    
     status_file = open(data_folder_path / 'tracking_data' / f'status_{target_name}.txt', 'w').close()
     status_file = open(data_folder_path / 'tracking_data' / f'status_{target_name}.txt', 'a')
-
-    is_sound_enabled = True
-    
+    is_sound_enabled = True 
     last_status = 'offline'
     try:
 # After username is found it prints the various name if matching strings are there and asks the user for which name to track and sets that name to target.
         print(f'Tracking: {target_name}')
-# It writes the status whether offline or online in tracking file.
-        
+# It writes the status whether offline or online in tracking file.       
 # Checks for the status and plays the sound for confirmation
         status_file.write(f'Tracking: {target_name}\n')
         while True:
