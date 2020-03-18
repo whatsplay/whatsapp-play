d3acd2caff0637dc4d1225e31039b3d759906453
Meenakshi2000 <meenakshi.daisy@gmail.com>
2020-03-18 17:26:35 +0530

    added screenshots

---
+++ myscreen.py
@@ -0,0 +1,174 @@
+Microsoft Windows [Version 10.0.17134.1304]
+(c) 2018 Microsoft Corporation. All rights reserved.
+
+C:\Users\swati\Documents\GitHub\whatsapp-play>python -m pip install -r requirements.txt
+Requirement already satisfied: python-telegram-bot>=11.1.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 1)) (12.4.2)
+Requirement already satisfied: datetime>=4.3 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 2)) (4.3)
+Requirement already satisfied: argparse>=1.4.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 3)) (1.4.0)
+Requirement already satisfied: playsound>=1.2.2 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 4)) (1.2.2)
+Requirement already satisfied: beautifulsoup4>=4.8.1 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 5)) (4.8.2)
+Requirement already satisfied: pyppeteer>=0.0.25 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 6)) (0.0.25)
+Requirement already satisfied: whaaaaat>=0.5.2 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 7)) (0.5.2)
+Requirement already satisfied: pyfiglet>=0.8.post1 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 8)) (0.8.post1)
+Requirement already satisfied: prompt_toolkit==1.0.14 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 9)) (1.0.14)
+Requirement already satisfied: requests>=2.22.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 10)) (2.22.0)
+Requirement already satisfied: psutil>=5.7.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 11)) (5.7.0)
+Requirement already satisfied: flake8>=3.7.9 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt (line 12)) (3.7.9)
+Requirement already satisfied: transitions>=0.7.2 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from -r requirements.txt
+(line 13)) (0.7.2)
+Requirement already satisfied: tornado>=5.1 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (6.0.4)
+Requirement already satisfied: cryptography in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (2.8)
+Requirement already satisfied: certifi in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (2019.3.9)
+Requirement already satisfied: future>=0.16.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (0.18.2)
+Requirement already satisfied: decorator>=4.4.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (4.4.2)
+Requirement already satisfied: pytz in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from datetime>=4.3->-r requirements.txt (line 2)) (2019.3)
+Requirement already satisfied: zope.interface in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from datetime>=4.3->-r requirements.txt (line 2)) (4.7.2)
+Requirement already satisfied: soupsieve>=1.2 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from beautifulsoup4>=4.8.1->-r requirements.txt (line 5)) (2.0)
+Requirement already satisfied: pyee in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from pyppeteer>=0.0.25->-r requirements.txt (line 6)) (7.0.1)
+Requirement already satisfied: websockets in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from pyppeteer>=0.0.25->-r requirements.txt (line 6)) (8.1)
+Requirement already satisfied: appdirs in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from pyppeteer>=0.0.25->-r requirements.txt (line 6)) (1.4.3)
+Requirement already satisfied: urllib3 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from pyppeteer>=0.0.25->-r requirements.txt (line 6)) (1.25.3)
+Requirement already satisfied: tqdm in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from pyppeteer>=0.0.25->-r requirements.txt (line 6)) (4.43.0)
+Requirement already satisfied: Pygments>=2.2.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from whaaaaat>=0.5.2->-r requirements.txt (line 7)) (2.6.1)
+Requirement already satisfied: regex>=2016.11.21 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from whaaaaat>=0.5.2->-r requirements.txt (line 7)) (2020.2.20)
+Requirement already satisfied: six>=1.9.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from prompt_toolkit==1.0.14->-r requirements.txt (line 9)) (1.12.0)
+Requirement already satisfied: wcwidth in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from prompt_toolkit==1.0.14->-r requirements.txt (line 9)) (0.1.8)
+Requirement already satisfied: idna<2.9,>=2.5 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from requests>=2.22.0->-r requirements.txt (line 10)) (2.8)
+Requirement already satisfied: chardet<3.1.0,>=3.0.2 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from requests>=2.22.0->-r requirements.txt (line 10)) (3.0.4)
+Requirement already satisfied: pyflakes<2.2.0,>=2.1.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from flake8>=3.7.9->-r requirements.txt (line 12)) (2.1.1)
+Requirement already satisfied: mccabe<0.7.0,>=0.6.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from flake8>=3.7.9->-r
+requirements.txt (line 12)) (0.6.1)
+Requirement already satisfied: pycodestyle<2.6.0,>=2.5.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from flake8>=3.7.9->-r requirements.txt (line 12)) (2.5.0)
+Requirement already satisfied: entrypoints<0.4.0,>=0.3.0 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from flake8>=3.7.9->-r requirements.txt (line 12)) (0.3)
+Requirement already satisfied: cffi!=1.11.3,>=1.8 in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from cryptography->python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (1.14.0)
+Requirement already satisfied: setuptools in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from zope.interface->datetime>=4.3->-r requirements.txt (line 2)) (40.6.2)
+Requirement already satisfied: pycparser in c:\users\swati\appdata\local\programs\python\python37-32\lib\site-packages (from cffi!=1.11.3,>=1.8->cryptography->python-telegram-bot>=11.1.0->-r requirements.txt (line 1)) (2.20)
+WARNING: You are using pip version 19.1.1, however version 20.0.2 is available.
+You should consider upgrading via the 'python -m pip install --upgrade pip' command.
+
+C:\Users\swati\Documents\GitHub\whatsapp-play>python -m wplay -h
+                 __
+ _      ______  / /___ ___  __
+| | /| / / __ \/ / __ `/ / / /
+| |/ |/ / /_/ / / /_/ / /_/ /
+|__/|__/ .___/_/\__,_/\__, /
+      /_/            /____/
+
+usage: __main__.py [-h] (-wc | -wb | -wti | -wt | -wtb | -pull) [TARGET]
+
+WhatsApp-play
+
+positional arguments:
+  TARGET          contact or group name, optional , target can be selected
+                  manually except for saving chat
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -wc, --wchat    chatting from command line
+  -wb, --wblast   message blast to a person
+  -wti, --wtimer  send messages from time to time
+  -wt, --wtrack   track online status of person
+  -wtb, --wtgbot  sends tracking status to telegram bot
+  -pull, --pull   save all chats, target is necessary
+Process Killed!
+
+C:\Users\swati\Documents\GitHub\whatsapp-play>python -m wplay -wt "Paps"
+                 __
+ _      ______  / /___ ___  __
+| | /| / / __ \/ / __ `/ / / /
+| |/ |/ / /_/ / / /_/ / /_/ /
+|__/|__/ .___/_/\__,_/\__, /
+      /_/            /____/
+
+? ***Session Manager***:  Continue without saving
+Looking for: Paps
+Contacts found:
+0: Paps
+Enter the number of the target you wish to choose:
+Process Killed!
+Traceback (most recent call last):
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\runpy.py", line 193, in _run_module_as_main
+    "__main__", mod_spec)
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\runpy.py", line 85, in _run_code
+    exec(code, run_globals)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 120, in <module>
+    asyncio.get_event_loop().run_until_complete(main())
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\asyncio\base_events.py", line 584, in run_until_complete
+    return future.result()
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 114, in main
+    await get_and_match_args(parser)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 82, in get_and_match_args
+    await onlinetracker.tracker(args.target)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\onlinetracker.py", line 15, in tracker
+    target_name = await target_search.search_and_select_target(page, target,
+hide_groups = True)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\utils\target_search.py", line 46, in search_and_select_target
+    target_index_choosed = __ask_user_to_choose_the_filtered_target(target_tuple)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\utils\target_search.py", line 307, in __ask_user_to_choose_the_filtered_target
+    input('Enter the number of the target you wish to choose: '))
+ValueError: invalid literal for int() with base 10: ''
+
+C:\Users\swati\Documents\GitHub\whatsapp-play>python -m wplay -wt "Paps"
+                 __
+ _      ______  / /___ ___  __
+| | /| / / __ \/ / __ `/ / / /
+| |/ |/ / /_/ / / /_/ / /_/ /
+|__/|__/ .___/_/\__,_/\__, /
+      /_/            /____/
+
+? ***Session Manager***:  Continue without saving
+Looking for: Paps
+Contacts found:
+0: Paps
+Enter the number of the target you wish to choose: 0
+No groups in common
+Name: Paps
+Last_seen: last seen today at 4:35 PM
+About: Available
+Mobile: +91 98104 48738
+Groups:
+
+Tracking: Paps
+18/03/2020, 16:42:39 - Status: offline
+
+Status file saved in: C:\Users\swati\wplay\tracking_data\status_Paps.txt
+Process Killed!
+Traceback (most recent call last):
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\runpy.py", line 193, in _run_module_as_main
+    "__main__", mod_spec)
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\runpy.py", line 85, in _run_code
+    exec(code, run_globals)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 120, in <module>
+    asyncio.get_event_loop().run_until_complete(main())
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\asyncio\base_events.py", line 584, in run_until_complete
+    return future.result()
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 114, in main
+    await get_and_match_args(parser)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\__main__.py", line 82, in get_and_match_args
+    await onlinetracker.tracker(args.target)
+  File "C:\Users\swati\Documents\GitHub\whatsapp-play\wplay\onlinetracker.py", line 48, in tracker
+    time.sleep(0.5)
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\site-packages\pyppeteer\launcher.py", line 174, in _close_process
+    self._loop.run_until_complete(self.killChrome())
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\asyncio\base_events.py", line 571, in run_until_complete
+    self.run_forever()
+  File "C:\Users\swati\AppData\Local\Programs\Python\Python37-32\lib\asyncio\base_events.py", line 526, in run_forever
+    raise RuntimeError('This event loop is already running')
+RuntimeError: This event loop is already running
+
+C:\Users\swati\Documents\GitHub\whatsapp-play>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
