c4cf84b13ed692cbf19e6fd95f80f7d6a66cd128
iharshit009 <harshitjain1309@gmail.com>
2020-03-24 13:57:31 +0530

    updated repo

--- wplay/changeAbout.py
+++ wplay/changeAbout.py
@@ -1,5 +1,5 @@
-from wplay.utils.helpers import whatsapp_selectors_dict
-from wplay.utils import browser_config
+# from wplay.utils.helpers import whatsapp_selectors_dict
+# from wplay.utils import browser_config
 import time
 import sys
 import requests
@@ -32,8 +32,12 @@ def get_live_cases():
     '''
     Gives total cases live in world for coronavirus(IN)
     '''
-    URL = "https://thevirustracker.com/free-api?global=stats"
-    data = requests.get(URL).json()
+    # URL = "https://thevirustracker.com/free-api?global=stats"
+    # data = requests.get(URL).json()
 
-    live_cases = data['results'][0]['total_cases']
-    return str("Total Coronavirus Cases: " + str(live_cases))
+    # live_cases = data['results'][0]['total_cases']
+    lst = ['h','ha','har','hars','harsh','harshi','harshit']
+    for i in range(len(lst)):
+        return( lst[i])
+
+print(get_live_cases())
