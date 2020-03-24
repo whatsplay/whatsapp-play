26c756878400e16ddceafc8da0f8395ffacb51f4
iharshit009 <harshitjain1309@gmail.com>
2020-03-24 14:01:09 +0530

    resolved changes to changeabout

--- wplay/changeAbout.py
+++ wplay/changeAbout.py
@@ -1,12 +1,14 @@
-# from wplay.utils.helpers import whatsapp_selectors_dict
-# from wplay.utils import browser_config
+from wplay.utils.helpers import whatsapp_selectors_dict
+from wplay.utils import browser_config
 import time
 import sys
 import requests
 
+
 async def changeAbout():
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
-    choice = input("Choose 1 for changing About section every second and 2 for changing About in every 30 min : ")
+    choice = input(
+        "Choose 1 for changing About section every second and 2 for changing About in every 30 min : ")
     if choice not in ['1', '2']:
         print("Wrong choice")
         sys.exit()
@@ -28,16 +30,14 @@ async def changeAbout():
         else:
             time.sleep(1800)
 
+
 def get_live_cases():
     '''
     Gives total cases live in world for coronavirus(IN)
     '''
-    # URL = "https://thevirustracker.com/free-api?global=stats"
-    # data = requests.get(URL).json()
+    URL = "https://thevirustracker.com/free-api?global=stats"
+    data = requests.get(URL).json()
 
-    # live_cases = data['results'][0]['total_cases']
-    lst = ['h','ha','har','hars','harsh','harshi','harshit']
-    for i in range(len(lst)):
-        return( lst[i])
+    live_cases = data['results'][0]['total_cases']
 
-print(get_live_cases())
+    return str("Total Coronavirus Cases: " + str(live_cases))
