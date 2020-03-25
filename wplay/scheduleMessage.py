c58023f2db8fa7e579163dd465eda765ba473fa6
Rits1272 <ritikjain1272@gmail.com>
2020-03-25 20:33:26 +0530

    change

--- wplay/scheduleMessage.py
+++ wplay/scheduleMessage.py
@@ -12,17 +12,19 @@ async def schedule_message(target):
         await target_search.search_and_select_target(page, target)
     else:
         await target_select.manual_select_target(page)
+    
     time_ = input("Enter the schedule time in HH:MM:SS format-> ")
     hour, minute, second = time_.split(':')
     current_time = datetime.now()
-    delta_hour : int = int(hour) - current_time.hour
-    delta_min : int = int(minute) - current_time.minute
-    delta_second : int = int(second) - current_time.second
-    total_seconds : int = delta_hour*3600 + delta_min*60 + delta_second
+    delta_hour = int(hour) - current_time.hour
+    delta_min = int(minute) - current_time.minute
+    delta_second = int(second) - current_time.second
+    total_seconds = delta_hour*3600 + delta_min*60 + delta_second
     if total_seconds < 0:
         print("Current time is ahead of the scheduled time")
         sys.exit()
-    message : list[str] = io.ask_user_for_message_breakline_mode()
+    message = io.ask_user_for_message_breakline_mode()
     print("Your message is scheduled at : ", time_)
+    
     time.sleep(total_seconds)
     await io.send_message(page, message)
