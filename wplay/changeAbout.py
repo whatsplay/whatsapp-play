d75d664d6cbda4e14f8e4f0325fa760102459a83
Rits1272 <ritikjain1272@gmail.com>
2020-03-31 14:50:00 +0530

    Display all news in changeAbout

--- wplay/changeAbout.py
+++ wplay/changeAbout.py
@@ -1,43 +1,41 @@
 from wplay.utils.helpers import whatsapp_selectors_dict
 from wplay.utils import browser_config
+from newsapi.newsapi_client import NewsApiClient
 import time
+import os
 import sys
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
 async def changeAbout():
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
-    choice = input(
-        "Choose 1 for changing About section every second and 2 for changing About in every 30 min : ")
-    if choice not in ['1', '2']:
-        print("Wrong choice")
-        sys.exit()
+    query = input("What's the news theme? : ")
     await page.waitForSelector(
         whatsapp_selectors_dict['profile_photo_element'],
         visible=True
     )
     await page.click(whatsapp_selectors_dict['profile_photo_element'])
-
+    news = ''
     while True:
-        await page.waitForSelector(whatsapp_selectors_dict['about_edit_button_element'])
-        await page.click(whatsapp_selectors_dict['about_edit_button_element'])
-        for _ in range(140):
-            await page.keyboard.press('Backspace')
-        await page.type(whatsapp_selectors_dict['about_text_area'], get_live_cases())
-        await page.keyboard.press('Enter')
-        if choice == '1':
-            time.sleep(1)
-        else:
-            time.sleep(1800)
+        current_news = str(fetch_news(query))
+        print(current_news)
+        if news != current_news:
+            await page.waitForSelector(whatsapp_selectors_dict['about_edit_button_element'])
+            await page.click(whatsapp_selectors_dict['about_edit_button_element'])
+            for _ in range(140):
+                await page.keyboard.press('Backspace')
+            news = current_news
+            await page.type(whatsapp_selectors_dict['about_text_area'], news)
+            await page.keyboard.press('Enter')
+        # News get updated every 15 minutes by newsapi.org
+        # Added extra minute for buffer period
+        # For free account : max limit is 500 request/day
+        time.sleep(16*60)
 
-
-def get_live_cases():
-    '''
-    Gives total cases live in world for coronavirus(IN)
-    '''
-    URL = "https://thevirustracker.com/free-api?global=stats"
-    data = requests.get(URL, headers={"User-Agent": "XY"}).json()
-
-    live_cases = data['results'][0]['total_cases']
-
-    return str("Total Coronavirus Cases: " + str(live_cases))
+def fetch_news(query):
+    top_headlines = newsapi.get_top_headlines(q=query, language='en')
+    return top_headlines['articles'][0]['title']
