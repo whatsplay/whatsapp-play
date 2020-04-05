9caef6f1c31cf8c5f02f82af1667ed701fb0dad6
Alexandre Calil <alexandrecalilmf@gmail.com>
2020-04-04 23:58:09 -0300

    add a new website to chat with unknown numbers

--- wplay/utils/helpers.py
+++ wplay/utils/helpers.py
@@ -7,7 +7,8 @@ from whaaaaat import style_from_dict, Token
 # endregion
 
 # region WEBSITES
-websites = {'whatsapp': 'https://web.whatsapp.com/'}
+websites = {'whatsapp': 'https://web.whatsapp.com/',
+            'wpp_unknown': 'https://web.whatsapp.com/send?phone='}
 # endregion
 
 # region SELECTORS
