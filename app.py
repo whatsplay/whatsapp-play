2dbe2f8cdd1cc83b4f17ddacfdccc0893254f784
Neelaksh Singh <56018436+Neelaksh-Singh@users.noreply.github.com>
2020-04-10 12:26:21 +0530

    wplaybot
    This is a wplaybot build using python and dialogflow, hosted by heroku and twilio.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from st import fetch_reply
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+
+    reply=fetch_reply(msg, phone_no)
+    # Create reply
+    resp = MessagingResponse()
+    resp.message(reply)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ st.py
@@ -0,0 +1,16 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "small-talk-hypdyq-853774a52200.json"
+
+import dialogflow_v2 as dialogflow
+dialogflow_session_client = dialogflow.SessionsClient()
+PROJECT_ID = "small-talk-hypdyq"
+def detect_intent_from_text(text, session_id, language_code='en'):
+    session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
+    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+    return response.query_result
+
+def fetch_reply(query ,session_id):
+    response = detect_intent_from_text(query ,session_id)
+    return response.fulfillment_text
