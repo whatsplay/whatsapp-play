6a62d6e30ea7cf3a4edab20f67cbbb85db041f9d
Himadri Singh <36266646+SinghHimadri@users.noreply.github.com>
2020-04-15 01:34:04 +0530

    #102 Automated unit test written for helper.py (#297)
    * Dockerfile updated
    
    * test for helper completed successfully running
    
    * test written successfully
    
    * Changes made
    
    * changes done

---
+++ tests/test_helpers.py
@@ -0,0 +1,26 @@
+'''
+To run tests run
+python3 -m unittest discover -s tests
+on terminal
+'''
+#region imports
+import unittest
+from os import path
+from wplay.utils.helpers import log_file_path, logs_path, data_folder_path, user_data_folder_path, profile_photos_path, tracking_folder_path
+#endregion
+
+
+#region class
+class testPath(unittest.TestCase):
+
+    def test_paths_exist(self):
+        self.assertTrue(path.exists(data_folder_path))
+        self.assertTrue(path.exists(logs_path))
+        self.assertTrue(path.exists(log_file_path))
+        self.assertTrue(path.exists(user_data_folder_path))
+        self.assertTrue(path.exists(profile_photos_path))
+        self.assertTrue(path.exists(tracking_folder_path))
+
+if __name__ == '__main__':
+	unittest.main()
+#endregion
