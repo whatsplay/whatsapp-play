67e8639910680b92355b318517fbb133147667b3
SinghHimadri <himsingh1612@gmail.com>
2020-04-16 22:54:14 +0530

    test log file created and format changed time and date removed

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,7 +6,7 @@ on terminal
 #region imports
 import unittest
 from os import path
-from wplay.utils.helpers import log_file_path, logs_path, data_folder_path, user_data_folder_path, profile_photos_path, tracking_folder_path
+from wplay.utils.helpers import log_file_path, logs_path,test_log_file_path, data_folder_path, user_data_folder_path, profile_photos_path, tracking_folder_path
 #endregion
 
 
@@ -17,6 +17,7 @@ class testPath(unittest.TestCase):
         self.assertTrue(path.exists(data_folder_path))
         self.assertTrue(path.exists(logs_path))
         self.assertTrue(path.exists(log_file_path))
+        self.assertTrue(path.exists(test_log_file_path))
         self.assertTrue(path.exists(user_data_folder_path))
         self.assertTrue(path.exists(profile_photos_path))
         self.assertTrue(path.exists(tracking_folder_path))
--- wplay/utils/Logger.py
+++ wplay/utils/Logger.py
@@ -1,7 +1,7 @@
 # region IMPORTS
 import logging
 from pathlib import Path
-from wplay.utils.helpers import log_file_path, logs_path
+from wplay.utils.helpers import log_file_path, logs_path, test_log_file_path
 # endregion
 
 
@@ -9,6 +9,10 @@ class Logger:
     if not (log_file_path).exists():
         logs_path.mkdir(parents=True, exist_ok=True)
         open(log_file_path, 'w').close()
+
+    if not (test_log_file_path).exists():
+        logs_path.mkdir(parents=True, exist_ok=True)
+        open(test_log_file_path, 'w').close()
 
     def __init__(self, script_name: str, level: int = logging.WARNING):
         self.logger = logging.getLogger(script_name)
@@ -18,13 +22,14 @@ class Logger:
         if not self.logger.handlers:
             # Create handlers
             file_handler = logging.FileHandler(log_file_path)
+            file_handler = logging.FileHandler(test_log_file_path)
             console = logging.StreamHandler()
             file_handler.setLevel(self.level)
             console.setLevel(self.level)
 
             # create formatter and add it to the handlers
             formatter = logging.Formatter(
-                    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                    '%(name)s - %(levelname)s - %(message)s'
                     )
             console.setFormatter(formatter)
             file_handler.setFormatter(formatter)
