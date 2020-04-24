9c1f36d7f97d64cbf555ce62634e476594fcc487
SinghHimadri <himsingh1612@gmail.com>
2020-04-24 19:09:52 +0530

    code improved

--- wplay/location_finder.py
+++ wplay/location_finder.py
@@ -15,12 +15,6 @@ from wplay.utils.Logger import Logger
 __logger = Logger(Path(__file__).name)
 # endregion
 
-code_info = '[*] '
-code_warning = '(!) '
-code_result = '[+] '
-code_error = '[!] '
-code_title = ''
-
 number = '' # Full number format
 localNumber = '' # Local number format
 internationalNumber = '' # International numberformat
@@ -37,7 +31,7 @@ def localScan(InputNumber):
     global numberCountryCode
     global numberCountry
 
-    print(code_info + 'Running local scan...')
+    print('Running local scan...')
     FormattedPhoneNumber = "+" + formatNumber(InputNumber)
     PhoneNumberObject = phonenumbers.parse(FormattedPhoneNumber, None)
 
@@ -53,24 +47,24 @@ def localScan(InputNumber):
     localNumber = phonenumbers.format_number(PhoneNumberObject, phonenumbers.PhoneNumberFormat.E164).replace(numberCountryCode, '')
     internationalNumber = phonenumbers.format_number(PhoneNumberObject, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
 
-    print(code_result + 'International format: {}'.format(internationalNumber))
-    print(code_result + 'Local format: 0{}'.format(localNumber))
-    print(code_result + 'Country code: {}'.format(numberCountryCode))
-    print(code_result + 'Location: {}'.format(geocoder.description_for_number(PhoneNumberObject, "en")))
-    print(code_result + 'Carrier: {}'.format(carrier.name_for_number(PhoneNumberObject, 'en')))
-    print(code_result + 'Area: {}'.format(geocoder.description_for_number(PhoneNumberObject, 'en')))
+    print('International format: {}'.format(internationalNumber))
+    print('Local format: 0{}'.format(localNumber))
+    print('Country code: {}'.format(numberCountryCode))
+    print('Location: {}'.format(geocoder.description_for_number(PhoneNumberObject, "en")))
+    print('Carrier: {}'.format(carrier.name_for_number(PhoneNumberObject, 'en')))
+    print('Area: {}'.format(geocoder.description_for_number(PhoneNumberObject, 'en')))
     for timezoneResult in timezone.time_zones_for_number(PhoneNumberObject):
-        print(code_result + 'Timezone: {}'.format(timezoneResult))
+        print('Timezone: {}'.format(timezoneResult))
 
     if phonenumbers.is_possible_number(PhoneNumberObject):
-        print(code_info + 'The number is valid and possible.')
+        print('The number is valid and possible.')
     else:
-        print(code_warning + 'The number is valid but might not be possible.')
+        print('The number is valid but might not be possible.')
 
 
 
 def scanNumber(InputNumber):
-    print(code_title + "[!] ---- Fetching informations for {} ---- [!]".format(formatNumber(InputNumber)))
+    print("[!] ---- Fetching informations for {} ---- [!]".format(formatNumber(InputNumber)))
 
     localScan(InputNumber)
 
@@ -81,7 +75,7 @@ def scanNumber(InputNumber):
     global numberCountry
 
     if not number:
-        print((code_error + "Error: number {} is not valid. Skipping.".format(formatNumber(InputNumber))))
+        print(("Error: number {} is not valid. Skipping.".format(formatNumber(InputNumber))))
         sys.exit()
 
     print("Scan finished.")
