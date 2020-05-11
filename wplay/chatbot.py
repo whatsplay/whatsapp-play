0db2ffef9d5cafe71bbde9f6ad5d06ddabb612bb
SinghHimadri <himsingh1612@gmail.com>
2020-05-11 18:12:41 +0530

    google search added

--- wplay/chatbot.py
+++ wplay/chatbot.py
@@ -1,6 +1,7 @@
+from googlesearch import search
 def Bot(last_Message):
     print('\n Bot activated')
-    last_Message= "".join(last_Message.split())
+    first_last_Message= "".join(last_Message.split())
     simple_menu = {                                 # function requires no extra arguments
                 "hi": say_hi,
                 "help": _help_commands,
@@ -9,13 +10,22 @@ def Bot(last_Message):
                 "howareyou?": say_fine,
             }
     simple_menu_keys = simple_menu.keys()
+    result = []
 
     try:
-        command_args = last_Message[1:].split(" ", 1)
-        print("Command args: {cmd}".format(cmd=command_args))
+        command_args = first_last_Message[1:].split(" ", 1)
+        command_arg = last_Message[1:].split(" ", 1)
 
         if len(command_args) == 1 and command_args[0] in simple_menu_keys:
             return simple_menu[command_args[0]]()
+
+        elif command_arg[0] == 'google':
+            query = "".join(command_arg[1])
+            for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+                result.append(j)
+            print("Sending links for query")
+            return result
+
         else:
             return "Wrong command. Send me /help to see a list of valid commands"
 
