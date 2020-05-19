d10749dfa5b081774f67c99bc3f592ddf3fa8c33
Himadri Singh <36266646+SinghHimadri@users.noreply.github.com>
2020-05-20 01:34:21 +0530

    get_news working successfully now. (#346)
    * unboundlocalerror corrected
    
    * Code improved and readme updated
    
    * issues resolved
    
    * issue resolved
    
    * issues resolved
    
    * issue resolved

--- wplay/get_news.py
+++ wplay/get_news.py
@@ -1,9 +1,7 @@
 # region IMPORTS
 from pathlib import Path
 import time
-import os
 
-from dotenv import load_dotenv
 from newsapi.newsapi_client import NewsApiClient
 
 from wplay.utils import browser_config
@@ -18,10 +16,10 @@ from wplay.utils.Logger import Logger
 __logger = Logger(Path(__file__).name)
 # endregion
 
-
-load_dotenv()
-newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
-
+'''
+Visit https://newsapi.org/ to get your own API key.
+'''
+newsapi = NewsApiClient(api_key="YOUR API KEY")
 
 async def get_news(target):
     def fetch_news(country_code):
@@ -45,7 +43,7 @@ async def get_news(target):
         try:
             news, source = fetch_news(country)
             news_ = f"*{news}* \n Full News :  {source}"
+            await io.send_message(page, news_)
         except Exception as e:
             print("Unable to get the news", e)
-        await io.send_message(page, news_)
-        time.sleep(900)  # Sends news in every 15 min
+        time.sleep(120)  # Sends news in every 2 min
