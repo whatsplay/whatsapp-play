0995f2cbfc1f5a3a4ebb9ebfe0dd9a0f2eb9b979
Himadri Singh <36266646+SinghHimadri@users.noreply.github.com>
2020-05-26 03:22:58 +0530

    Text to speech added successfully. (#358)
    * text to speech added
    
    * comments added
    
    * logger added
    
    * requirements updated
    
    * keyword updated
    
    * error corrected
    
    * issues resolved
    
    * requirements updated
    
    * commnets added

--- wplay/terminal_chat.py
+++ wplay/terminal_chat.py
@@ -5,6 +5,7 @@ from wplay.utils import browser_config
 from wplay.utils import target_search
 from wplay.utils import target_select
 from wplay.utils import io
+from wplay import text_to_speech
 from wplay.utils.Logger import Logger
 from wplay.utils.helpers import logs_path
 from colorama import Fore, Style
@@ -31,7 +32,7 @@ async def chat(target):
     else:
         target = await target_select.manual_select_target(page)
 
-    print("\033[91m {}\033[00m".format("\nType '...' in a new line or alone in the message to change target person.\nType '#_FILE' to send Image/Video/Documentd etc.\n"))
+    print("\033[91m {}\033[00m".format("\nType '...' in a new line or alone in the message to change target person.\nType '#_FILE' to send Image/Video/Documentd etc.\nType '#_TTS' to convert text to speech and send audio file.\n"))
 
     while True:
         await getMessages(page, target)
@@ -46,6 +47,11 @@ async def chat(target):
             else:
                 await target_select.manual_select_target(page)
             message = io.ask_user_for_message_breakline_mode()
+
+        #Text to speech
+        if '#_TTS' in message:
+            await text_to_speech.text_to_speech(target)
+            await io.send_file(page)
 
         # File Share:
         if '#_FILE' in message:
---
+++ wplay/text_to_speech.py
@@ -0,0 +1,41 @@
+# region IMPORTS
+from pathlib import Path
+
+from wplay.utils.helpers import audio_file_folder_path
+from wplay.utils.Logger import Logger
+
+from gtts import gTTS
+# endregion
+
+
+# region LOGGER
+__logger = Logger(Path(__file__).name)
+# endregion
+
+async def text_to_speech(target):
+
+    try:
+        __logger.info("Converting text to speech audio file")
+        # The text that you want to convert to audio
+        text = input("\n\nWrite the text you want to convert to audio file: ")
+
+        list_laguages=['bn: Bengali', 'de: German', 'en: English','es: Spanish','fr: French','gu: Gujarati','hi: Hindi',
+        'it: Italian','ja: Japanese','kn: Kannada','ko: Korean','ml: Malayalam','mr: Marathi','pt-br: Portuguese (Brazil)','ru: Russian',
+        'ta: Tamil','te: Telugu','ur: Urdu',]
+
+        print('Choose a code for language of your choice from the following list\n')
+        print(list_laguages)
+
+        # Language in which you want to convert
+        language = input("\n\nEnter the language you want to convert to audio file: ")
+
+        # Passing the text and language to the engine,
+        myobj = gTTS(text=text, lang=language, slow=False)
+        # Saving the converted audio in a mp3 file named
+        myobj.save( audio_file_folder_path / "{}.mp3".format(target))
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        print('\nAudio file saved in: {}/ {}.mp3'.format(audio_file_folder_path, target))
