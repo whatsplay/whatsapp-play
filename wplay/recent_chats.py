ff7343a82f7509ca3a17209edcec5152637b33f6
SinghHimadri <himsingh1612@gmail.com>
2020-05-30 20:17:19 +0530

    prit correctly

--- wplay/recent_chats.py
+++ wplay/recent_chats.py
@@ -5,6 +5,11 @@ async def recent_chat():
     await page.waitForSelector(selector)
     values = await page.evaluate(f'''() => [...document.querySelectorAll('{selector}')]
                                                     .map(element => element.textContent)''')
-    print(values)
     new_values = '\n'.join([' '.join(i) for i in zip(*[iter(values)]*5)])
     print(new_values)
+    print("break")
+    final = [' '.join(i) for i in zip(*[iter(values)]*5)]
+    reversed_list = final[::-1]
+    reversed_list = reversed_list[-1:] + reversed_list[:-1]
+    print(*reversed_list, sep = "\n")
+    #print(reversed_list)
