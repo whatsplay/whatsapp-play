d50e6535067c2a5aa5f042fdd97d2dc90af8b3e7
SinghHimadri <himsingh1612@gmail.com>
2020-05-30 19:47:02 +0530

    recent chat printing successfully

--- wplay/recent_chats.py
+++ wplay/recent_chats.py
@@ -1,14 +1,10 @@
 from wplay.utils import browser_config
 async def recent_chat():
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
-    selector = '#pane-side > div > div > div > div > div > div > div'
+    selector = '#pane-side > div > div > div > div > div > div > div > div > div'
     await page.waitForSelector(selector)
     values = await page.evaluate(f'''() => [...document.querySelectorAll('{selector}')]
                                                     .map(element => element.textContent)''')
-    str_list = list(filter(None, values))
-    print(str_list)
-    b = [s.split('Monday') for s in str_list]
-    print(b)
-    print("new")
-    new=[i.split('Monday', 1)[0] for i in str_list]
-    print(new)
+    print(values)
+    new_values = '\n'.join([' '.join(i) for i in zip(*[iter(values)]*5)])
+    print(new_values)
