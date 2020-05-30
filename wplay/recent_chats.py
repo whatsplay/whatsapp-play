1de2220655e130f83ffaf76c8dc948e692884a42
SinghHimadri <himsingh1612@gmail.com>
2020-05-30 20:41:13 +0530

    logger added

--- wplay/recent_chats.py
+++ wplay/recent_chats.py
@@ -1,5 +1,18 @@
+# region IMPORTS
+from pathlib import Path
+
 from wplay.utils import browser_config
+from wplay.utils.Logger import Logger
+# endregion
+
+
+# region LOGGER
+__logger = Logger(Path(__file__).name)
+# endregion
+
+
 async def recent_chat():
+    __logger.info("Fetching recent chats...")
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
     selector = '#pane-side > div > div > div > div > div > div > div > div > div'
     await page.waitForSelector(selector)
