2085c52c407f1672e048ed1871b01b18911ce765
SinghHimadri <himsingh1612@gmail.com>
2020-05-30 20:27:31 +0530

    code readibility improved and unwanted lines removed

--- wplay/recent_chats.py
+++ wplay/recent_chats.py
@@ -3,13 +3,9 @@ async def recent_chat():
     page, _ = await browser_config.configure_browser_and_load_whatsapp()
     selector = '#pane-side > div > div > div > div > div > div > div > div > div'
     await page.waitForSelector(selector)
-    values = await page.evaluate(f'''() => [...document.querySelectorAll('{selector}')]
+    recentChat_list = await page.evaluate(f'''() => [...document.querySelectorAll('{selector}')]
                                                     .map(element => element.textContent)''')
-    new_values = '\n'.join([' '.join(i) for i in zip(*[iter(values)]*5)])
-    print(new_values)
-    print("break")
-    final = [' '.join(i) for i in zip(*[iter(values)]*5)]
-    reversed_list = final[::-1]
-    reversed_list = reversed_list[-1:] + reversed_list[:-1]
-    print(*reversed_list, sep = "\n")
-    #print(reversed_list)
+    new_recentChat_list = [' '.join(i) for i in zip(*[iter(recentChat_list)]*5)]
+    final_recentChat_list = new_recentChat_list[::-1]
+    final_recentChat_list = final_recentChat_list[-1:] + final_recentChat_list[:-1]
+    print(*final_recentChat_list, sep = "\n")
